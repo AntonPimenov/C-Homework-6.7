05eae5ed86009475f0ad834e9c5b9b1f0c48d9b4
Anton.Pimenov <toni.pimenov.1985@mail.ru>
2024-02-21 20:07:33 +0300

    update cmakelist and build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/anton/C-Homework-6.7
+# Build directory: /home/anton/C-Homework-6.7/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
